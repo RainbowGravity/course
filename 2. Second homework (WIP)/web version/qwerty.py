4678ee1f0b19024df36101868720083f24b74890
Ivan <89798605+RainbowGravity@users.noreply.github.com>
2021-09-06 23:05:01 +0300

    Update qwerty.py

--- 2. Second homework (WIP)/web version/qwerty.py
+++ 2. Second homework (WIP)/web version/qwerty.py
@@ -1,37 +1,46 @@
+from emoji.core import emojize
 from flask import Flask, request, render_template
 import emoji
 import json
 
 def jsonc(data):
-    return emoji.emojize('%s says %s \n' % (data['animal'], data['sound'])) * (data['count']) \
-           + "\nMade with pyChamp by RainbowGravity\n"
+    return emoji.emojize('Done! :OK_hand:\n\n') + emoji.emojize(('%s says %s \n' % (data['animal'], data['sound']))) * (data['count']) \
+           + emoji.emojize("\nMade with VS Code by :rainbow:Gravity\n")
 
-app = Flask(__name__)
+def error():
+    return emoji.emojize(':red_exclamation_mark: Error: Incorrect data request. :red_exclamation_mark:\n\nPlease, enter the correct JSON data.' \
+        '\n\n:hollow_red_circle: Data format: :hollow_red_circle:\n\n{"animal": "value", "sound": "value", "count": number}\n')
 
-@app.route("/", methods = ['POST','GET'])
 def curl():
     try:
         return jsonc(data=request.get_json(force=True))
     except:
-        return 'Error! Please, enter correct data.\n\nData format:\n{"animal": "value","sound": "value","count": number}. \n If you have opened this from web browser, use https://localhost/web'
+        return error()
 
-@app.route("/web", methods = ['POST','GET'])
+def get():
+    try:
+        return render_template("web.html", result=jsonc(data=json.loads(request.args.get('js'))) + '\nMethod: GET')
+    except:
+        return render_template("web.html", result=error())
+
+
+def post():
+    try:
+        return render_template("web.html", result=jsonc(data=json.loads(request.form['js'])) + '\nMethod: POST')
+    except:
+        return render_template("web.html", result=error())
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['POST','GET'])
 def result():
-   if request.method == 'POST':
-        try:
-            return render_template("web.html", result=jsonc(data = json.loads(request.form['js'])) + '\nMethod: POST')
-        except:
-            return render_template("web.html", result= "Incorrect data request.\n\n Send correct JSON data.")
+   if request.user_agent.browser == None:
+        return curl()
    else:
-       try:
-           return render_template("web.html", result=jsonc(data=json.loads(request.args.get('js'))) + '\nMethod: GET')
-       except:
-           return render_template("web.html", result="Incorrect data request.\n\n Send correct JSON data.")
-
-@app.route("/about", methods = ['POST','GET'])
-def about():
-    return "\nNice to see you there! This is my first python web app.\n\nMade with pyChamp, built with Docker and " \
-           "deployed with Ansible by RainbowGravity.\n\nAndersen DevOps course, Saint Petersburg, 2021\n\n "
-
+        if request.method == 'POST':
+            return post()
+        else:
+            return get()
+            
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'), debug=False)
