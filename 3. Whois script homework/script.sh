41085f106134b1623f9f779c69417242ead61711
RainbowGravity <reinerrubin2020@gmail.com>
2021-10-03 02:41:31 +0300

    Imrovements and fixes for whois script.

--- 3. Whois script homework/script.sh
+++ 3. Whois script homework/script.sh
@@ -3,22 +3,24 @@
 		{
 			echo '	_____________________________________________________________________________________		'
 			echo ''
-			echo '	This script is used to read information about where is your processes connected to.'
-			echo '	You can enter a process name or its PID using -n option.'
-			echo '	You also can enable displaying of the additional information about amount of connections, its types'
-			echo '	status info, local address and peer info. '
-			echo '	'
-			echo '	Options by default:'
-			echo '	-n -- not specified. All of the IP addresses of the all processes will be displayed. '
-			echo '	-p -- "netname,address,country". You can enter anything in any order, but follow the same syntax.'
-			echo '	-s -- 5. Last 5 adresses will be processed. You can choose any amount. (Maybe not really any)'
-			echo '	-a -- additional info will not be displayed by default.'
-			echo '	'
-			echo '	Options:'
-			echo '	-n -- process name or ist PID'
-			echo '	-p -- paragraphs from whois which you want to be displayed about the IP address that chosen process using.'
-			echo '	-s -- the amount of the IP addresses which will be processed. You can choose any amount, but script will display not more than actual connections is present.'
-			echo '  -f -- filter by state, must be specified as for the ss: established, syn-sent, syn-recv, fin-wait-1, fin-wait-2, time-wait, closed, close-wait, last-ack, listening and closing '
+			echo 'This script is used to read information about where is your processes connected to.'
+			echo 'You can enter a process name or its PID using -n option.'
+			echo 'You also can enable displaying of the additional information about amount of connections, its types'
+			echo 'status info, local address and peer info. '
+			echo ''
+			echo 'Options by default:'
+			echo '-n -- not specified. All of the IP addresses of the all processes will be displayed. '
+			echo '-p -- "organization,netname". You can enter anything in any order, but follow the same syntax.'
+			echo '-s -- 5. 5 connections will be displayed. You can choose any amount.'
+			echo '-a -- additional info will not be displayed by default.'
+			echo ''
+			echo 'Options:'
+			echo '-n -- process name or ist PID'
+			echo '-p -- paragraphs from whois which you want to be displayed about the IP address that chosen process using.'
+			echo '-s -- the amount of connections which will be displayed for organizations. And for all connections if -a flag is present'
+			echo 'You can choose any amount, but script will display not more than actual connections is present.'
+			echo '-f -- filter by state, must be specified as for the ss: established, syn-sent, syn-recv, fin-wait-1, fin-wait-2, time-wait, closed, close-wait, last-ack, listening and closing '
+			echo '-a -- display all connections even without organization info from whois'
 			echo '	_____________________________________________________________________________________'
 			echo
 		}
@@ -41,16 +43,16 @@
 	# Header of the script output
 		Header()
 		{
-			printf '____________________________________________\n\n Information from whois about '$ip1':\n\n'
+			printf '============================================================\nInformation from whois about '$ip1':\n\n'
 		}
 	# Field of the additional information about IP
 		Additional()
 		{
 				ss state $state -p dst $ip1 | grep -v "Netid" | grep -q -i -E '[a-z]|[0-9]' && \
-				{ echo -e "\t\nInformation about IP from ss:\n" ;   \
-				ss state $state -p dst  $ip1 | awk -v OFS='|' '{print $0}' | grep -v "Netid" | sed -e 's/users:((//' -e 's/))//'; } ||\
+				{ echo -e "Information about IP from ss:\n" ;   \
+				ss state $state -p dst  $ip1 | awk -v OFS='|' '{print $0}' | grep -v "Netid" | sed -e 's/ [[:digit:]]\s\+[[:digit:]]     //' -e 's/users:((//' -e 's/))//'; } ||\
 				{ printf ' Additional information:\n' ;\
-				printf '\n\033[1;31mUnfortunately, connection was closed or terminated while script was processing other connections. \nIt may be caused by the ping to whois or process was terminated too. \nNo information from ss to display.\033[0m\n\n'; }
+				printf '\n\033[0;31mUnfortunately, connection was closed or terminated while script was processing other connections. \nIt may be caused by the ping to whois or process was terminated too. \nNo information from ss to display.\033[0m\n\n'; }
 		}
 		
 	# Checking for the paragraphs in the whois info
@@ -62,22 +64,37 @@
 	# Displaying information about amount of the connections displayed (yeah)
 		Counter()
 		{
-			printf "____________________________________________\n\n"
-			ca=$(echo $ips | wc -w)
-			sa=$(echo $ips | awk '{NF='$sa'}1' | wc -w)
-			
+			printf "============================================================\n"
+			ca=$(echo $ips | wc -w)			
 			ss -ptua state $state | awk '{print $'$qwe'}' | grep -v '0.0.0.0' | grep -q '[0-9]' && \
-			{ echo $na | grep -q -i -E '[a-z]|[0-9]' && printf '\033[1;32m Done! Displayed '$sa'/'$ca' connections for "'$na'" process and "'$state'" state. \033[0m\n\n' ||\
-			printf '\033[1;32m Done! Displayed '$sa'/'$ca' connections of all processes and "'$state'" state. \033[0m\n\n'; } || \
+			{ echo $na | grep -q -i -E '[a-z]|[0-9]' && printf '\033[1;32m Done! Processed '$cc'/'$ca' connections, displayed '$cb'/'$ca' for "'$na'" process and "'$state'" state. \033[0m\n\n' ||\
+			printf '\033[1;32m Done! Processed '$cc'/'$ca' connections, displayed '$cb'/'$ca' of all processes and "'$state'" state. \033[0m\n\n'; } || \
 			printf '\033[1;31m No connections was found for "'$na'" and "'$state'" state \033[0m\n\n'
 		}
+		Processing(){
+			while [ $p -gt 0 ]
+                do
+               	 	pa2=$(echo $pa | sed -e 's#,# #g' | awk '{print $'$p''})
+					Gerror
+                	((p--))
+			done
+			echo
+			cat /tmp/ss_script.tmp | grep -i "^organization:" >> /tmp/ss_script_organizations.tmp && ((sb++))
+			Additional
+		}
 	# Default options
-qwe="6"
-pa=$(echo "netname,address,country")
 sa="5"
+cb="0"
+qwe="6"
+sb="0"
+pa=$"organization,netname"
 state="all"
-while getopts "as:p:n:h:f:" option; do
+org="True"
+while getopts "as:p:n:hf:a" option; do
 	case $option in
+	a)
+		org="False"
+		;;	
 	f)
 		state=$OPTARG
 		qwe="5"
@@ -111,12 +128,12 @@ done
 	# Counting the real amount of the IP adresses that in use by the process
 ips=$(ss -ptua state $state | grep -i ''$na''| awk '{print $'$qwe'}' | grep -oP '(\d+\.){3}\d+' | sort | uniq -c | sort -r |\
 awk '{print $2}' | grep -v '0.0.0.0')
-ca=$(echo $ips | awk '{NF='$sa'}1' | wc -w)
-
+ca=$(echo $ips | wc -w)
+touch /tmp/ss_script_organizations.tmp
 	# Processing the information about the IP address
-while [ $ca -gt 0 ]
+while [ $ca -gt 0 ] && [ $sb -ne $sa ]
 	do
-	ip1=$(echo $ips | awk '{NF='$sa'}1' | awk '{print $'$ca'}')
+	ip1=$(echo $ips | awk '{NF='$ca'}1' | awk '{print $'$ca'}')
     whois $ip1 > /tmp/ss_script.tmp 
 	# Checking for paragraphs chosen
 		if [ "$pa" == "all" ];
@@ -125,20 +142,33 @@ while [ $ca -gt 0 ]
 			cat /tmp/ss_script.tmp
 			echo
 		else
-			Header
 			p=$(echo $pa | sed -e 's#,# #g' | wc -w)	
 	# Checking for errors in paragraps requests, responding with error if failed
-			while [ $p -gt 0 ]
-                do
-                pa2=$(echo $pa | sed -e 's#,# #g' | awk '{print $'$p''})
-				Gerror
-                ((p--))
-			done
-			echo
+			if [ $org == "True" ] 2>/dev/null
+				then 
+					cat /tmp/ss_script.tmp | grep -iq "^organization:" && \
+					{ Header;	Processing; ((cb++)); } || echo -n
+				else
+					Header
+					Processing
+					((cb++))
+					((sb++))
+			fi
 		fi
 	# Calling the function that will be display or not the additional info
-		Additional
+	((cc++))
 	((ca--))
 done
 Counter
+echo "Results for organization field:"
+echo
+echo "======================================"
+echo -e "№ Conn:\tOrganization name:"
+echo
+cat /tmp/ss_script_organizations.tmp | grep -iq "organization" && \
+{ cat /tmp/ss_script_organizations.tmp  | sed -e 's/organization:   //iI'| sort | uniq -c | sort -nr -k1,1; echo; } ||\
+echo -e "\033[1;31mThere is no organizations was found.\033[0m\n" 
+echo "======================================"
+echo
 rm /tmp/ss_script.tmp 2> /dev/null
+rm /tmp/ss_script_organizations.tmp 2> /dev/null
